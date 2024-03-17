00be52b51dc74e8c76114c0b8f86f0bf30c75ab4
Boloroo Geom <bolrochuluun2005@gmail.com>
2024-03-17 16:05:13 +0800

    init

---
+++ front/src/components/extra/order-detail/OrderDetailTab.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OrderDetailTab = () => {
+  return (
+    <div className="py-[18px] px-4 w-full bg-white h-fit flex flex-row items-center gap-6">
+      <img src="./ArrowLeft.png" alt="Arrow" className="w-5 h-5" />
+      <h3>Захиалгын дэлгэрэнгүй</h3>
+    </div>
+  );
+};
+
+export default OrderDetailTab;
--- front/src/components/extra/order/SingleOrderTable.tsx
+++ front/src/components/extra/order/SingleOrderTable.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
 
-const SingleOrderTable = () => {
+const SingleOrderTable = (props: {
+  id: string;
+  userName: string;
+  userEmail: string;
+  orderData: string;
+  orderTime: string;
+  orderPrice: string;
+  orderStatus: string;
+  img: string;
+}) => {
+  const {
+    id,
+    userName,
+    userEmail,
+    orderData,
+    orderTime,
+    orderPrice,
+    orderStatus,
+    img,
+  } = props;
   return (
-    <div>SingleOrderTable</div>
-  )
-}
+    <div className="w-full h-fit flex flex-row items-center justify-between py-4">
+      <div>{id}</div>
+      <div className="flex flex-col w-fit h-fit">
+        <p>{userName}</p>
+        <p>{userEmail}</p>
+      </div>
+      <div>{orderData}</div>
+      <div>{orderTime}</div>
+      <div>{orderPrice}</div>
+      <div>{orderStatus}</div>
+      <div>
+        <img src={`${img}`} alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default SingleOrderTable
+export default SingleOrderTable;
